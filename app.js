41c2a63d4fdf8692b6abf9711626a634da98549c
vishu221b <vishal.dogra738@gmail.com>
2020-07-30 10:57:30 +0530

    Finalising

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const request = require('request');
+const constants = require('./constants');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -10,11 +11,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
-
 let fetchQueryResultData = (query, callback) => {
     console.log('query is -> ', query);
     let finalResponse = [];
-    request(`https://api.themoviedb.org/3/search/movie?api_key=bbf5c216807a9cf1f1f3f2577210a8ad&query=${query}`, (error, response, responseBody) => {
+    request(`https://api.themoviedb.org/3/search/movie?api_key=${constants.KEY}&query=${query}`, (error, response, responseBody) => {
         if (error){
             console.log(error);
             return;
@@ -23,6 +23,7 @@ let fetchQueryResultData = (query, callback) => {
             respons = JSON.parse(responseBody);
             respons.results.forEach((entity) => {
                 finalResponse.push({
+                    id: '#' + entity.original_title.toString(),
                     title: entity.original_title,
                     summary: entity.overview,
                     poster: entity.poster_path,
@@ -39,21 +40,24 @@ let fetchQueryResultData = (query, callback) => {
 app.get('/results', (req, res)=>{
     let queryParam = req.query.movieName;
     console.log(queryParam);
-    fetchQueryResultData(queryParam, (movies) => {
-        console.log(movies);
-        console.log("Movies are this");
-        res.render('results', {movies: movies, query: queryParam});
-    });
+    if (queryParam){
+        fetchQueryResultData(queryParam, (movies) => {
+            console.log(movies);
+            console.log("Movies are this");
+            res.render('results', {movies: movies, query: queryParam});
+        });
+    }
 });
 
 app.get('/search', (req, res)=>{
     res.render('index');
 });
 
+app.get('/', (req, res) => {
+    res.redirect('/search');
+});
 
 
 app.listen(5000, ()=>{
     console.log("App fired up at port 5000.");
 });
-
-
